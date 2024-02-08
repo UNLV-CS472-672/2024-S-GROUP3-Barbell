ab1a9e9936f3ae20f420c563d9c1bce0649502b0
Thienguen <nvktvan@gmail.com>
2024-02-08 10:59:15 -0800

    🍳 fixes: minor changes mentioned by marcos

--- src/libs/util.ts
+++ src/libs/util.ts
@@ -2,8 +2,8 @@ import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 /**
- * @returns merge tailwind is a godsend of a utility
- * this place is throwing stuff inside
+ * @returns merged Tailwind CSS classes and removes 
+ * any duplicate classes while avoiding styling conflicts
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))

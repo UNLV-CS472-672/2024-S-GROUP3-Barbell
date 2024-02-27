e9c04bc82985a0b50053907a50b7fb0e65609d7a
Thienguen <nvktvan@gmail.com>
2024-02-27 15:36:49 -0800

    📚docs: env II

--- tooling/eslint/nextjs.js
+++ tooling/eslint/nextjs.js
@@ -10,7 +10,6 @@ const config = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-redundant-type-constituents": "off", // no fk clue
-    "@typescript-eslint/no-unused-vars": "off",
   },
 }
 

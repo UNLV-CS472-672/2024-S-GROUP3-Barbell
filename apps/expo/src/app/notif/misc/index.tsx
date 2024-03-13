1f2107063ebd1b2b9be710ae900ae52f5a9f96ac
Taylor Finelli <85365584+taylorfinelli@users.noreply.github.com>
2024-03-13 00:14:30 -0400

    Add functionality to buttons on notif/misc page. (#67)
    * Add functionality to buttons on notif/misc page. Buttons change colors and back arrow will pop back to previous page
    * Use ternary operators to set color instead of state hooks.
    * Use ternary operators to set color instead of state hooks.
    * removed unused import
    ---------
    Co-authored-by: Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>

--- apps/expo/src/app/notif/misc/index.tsx
+++ apps/expo/src/app/notif/misc/index.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { ScrollView } from 'react-native-gesture-handler';
+import { router } from 'expo-router'
+import MiscNotifs from '~/components/notif/miscNotifs';
+import DmNotifs from '~/components/notif/dmNotifs';
+import GcNotifs from '~/components/notif/gcNotifs';
+
+export type notifsType = "misc" | "dm" | "gc"
 
 export default function MiscNotifScreen() {
+  
+  const [visibleNotifs, setVisibleNotifs] = useState<notifsType>("misc");
+  
   return (
     <View style={{backgroundColor: "#1C1B1B", flex: 1}}>
       {/*header w/ back and new message buttons*/}
       <View className="flex flex-row justify-between pt-5 px-5">
-        <Ionicons name="chevron-back" size={24} color="#CACACA" />
+        <Ionicons onPress={() => router.back()} name="chevron-back" size={24} color="#CACACA" />
         <Text style={{color: "#CACACA", fontSize: 20}}>Notifications</Text>
         <MaterialCommunityIcons name="message-plus-outline" size={24} color="#CACACA" />
       </View>
 
       {/*notif nav buttons*/}
-      <View className="flex flex-row items-center m-2">
-        <View className="font-bold py-2 px-4 rounded-lg flex-1 ml-1 mr-1 mt-1" style={{backgroundColor: "#48476D"}}>
-          <Text style={{color: "#CACACA", textAlign: "center"}}>Notifications</Text>
-        </View>
-        <View className="font-bold py-2 px-4 rounded-lg flex-1 ml-1 mr-1 mt-1" style={{backgroundColor: "#CACACA"}}>
-          <Text style={{color: "#1C1B1B", textAlign: "center"}}>Direct</Text>
-        </View>
-        <View className="font-bold py-2 px-4 rounded-lg flex-1 ml-1 mr-1 mt-1" style={{backgroundColor: "#CACACA"}}>
-          <Text style={{color: "#1C1B1B", textAlign: "center"}}>Group</Text>
-        </View>
+      <View className="flex flex-row items-center m-2" >
+        <TouchableOpacity className="font-bold py-2 px-4 rounded-lg flex-1 ml-1 mr-1 mt-1" style={{backgroundColor: visibleNotifs == "misc" ? "#48476D" : "#CACACA"}}
+        onPress={() => {setVisibleNotifs("misc")}}>
+          <Text style={{color: visibleNotifs == "misc" ? "#CACACA" : "#1C1B1B", textAlign: "center"}}>Notifications</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="font-bold py-2 px-4 rounded-lg flex-1 ml-1 mr-1 mt-1" style={{backgroundColor: visibleNotifs == "dm" ? "#48476D" : "#CACACA"}}
+        onPress={() => {setVisibleNotifs("dm")}}>
+          <Text style={{color: visibleNotifs == "dm" ? "#CACACA" : "#1C1B1B", textAlign: "center"}}>Direct</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="font-bold py-2 px-4 rounded-lg flex-1 ml-1 mr-1 mt-1" style={{backgroundColor: visibleNotifs == "gc" ? "#48476D" : "#CACACA"}}
+        onPress={() => {setVisibleNotifs("gc")}}>
+          <Text style={{color: visibleNotifs == "gc" ? "#CACACA" : "#1C1B1B", textAlign: "center"}}>Group</Text>
+        </TouchableOpacity>
       </View>
 
       {/*thin line between notifs and nav section*/}
@@ -31,8 +43,11 @@ export default function MiscNotifScreen() {
 
       {/*list of notifications*/}
       <ScrollView className="flex-1">
-        {/*map notifications to notification component*/}
-        {/*will be ready once notification component and notification model are complete*/}
+        <View>
+          {visibleNotifs == "misc" && <MiscNotifs />}
+          {visibleNotifs == "dm" && <DmNotifs />}
+          {visibleNotifs == "gc" && <GcNotifs />}
+        </View>
       </ScrollView>
     </View>
   );

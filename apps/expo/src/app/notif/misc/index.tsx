568529258b49f1fde4073935cdcddc31f32e69be
Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>
2024-03-12 20:10:33 -0400

    removed unused import

--- apps/expo/src/app/notif/misc/index.tsx
+++ apps/expo/src/app/notif/misc/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import { ScrollView } from 'react-native-gesture-handler';

e2d63e03b4c4f0264fcf5579c23148cfb9e2d8e5
Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>
2024-03-14 18:02:32 -0400

    create timeago component, add isNaN error checking to calctime, add new message for empty notifs on miscnotifs.tsx. stopping point here as i am going to potentially add a new library at this point

--- apps/expo/src/components/notif/miscNotifs/miscNotifs.tsx
+++ apps/expo/src/components/notif/miscNotifs/miscNotifs.tsx
@@ -1,8 +1,9 @@
 import { View, Text, TouchableOpacity } from "react-native"
-import calculateTimeAgo from "~/utils/calculateTime";
 
 // friend request imports
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import TimeAgo from "~/components/timeAgo/TimeAgo";
+import { useEffect } from "react";
 
 const currentUTCTime = new Date().toISOString();
 
@@ -13,16 +14,16 @@ const notifications: notification[] = [
   {type: "misc", subType: "Friend request", content: "Marcos sent you a friend request", sender: "Marcos Villanueva", timeSent: "2024-03-14T18:30:01.202Z"},
   {type: "misc", subType: "Friend request", content: "Taylor sent you a friend request", sender: "Taylor Finelli", timeSent: "2024-03-14T17:30:01.202Z"},
   {type: "misc", subType: "Nudge", content: "Taylor nudged you", sender: "Taylor Finelli", timeSent: "2024-03-13T17:30:01.202Z"},
-  // {type: "misc", subType: "Friend request", content: "Stefano sent you a friend request", sender: "Stefano Rubini", timeSent: "2024-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Friend request", content: "Stefano sent you a friend request", sender: "Stefano Rubini", timeSent: "2024-03-10T17:30:01.202Z"},
   {type: "misc", subType: "Nudge", content: "Marcos nudged you", sender: "Marcos Villanueva", timeSent: "2024-03-5T17:30:01.202Z"},
   {type: "misc", subType: "Friend request", content: "Miguel sent you a friend request", sender: "Miguel Alvarez", timeSent: "2021-03-10T17:30:01.202Z"},
-  // {type: "misc", subType: "Friend request", content: "Thien sent you a friend request", sender: "This is a really long name for testing an edge case of overflowing text"},
-  // {type: "misc", subType: "Friend request", content: "Dan sent you a friend request", sender: "Daniel Hailu"},
-  // {type: "misc", subType: "Friend request", content: "Tom sent you a friend request", sender: "Tom Bryant"},
-  // {type: "misc", subType: "Nudge", content: "Miguel nudged you", sender: "Miguel Alvarez"},
-  // {type: "misc", subType: "Friend request", content: "Henry sent you a friend request", sender: "Henry Luong"},
-  // {type: "misc", subType: "Friend request", content: "Aaron sent you a friend request", sender: "Aaron Fung"},
-  // {type: "misc", subType: "Friend request", content: "Thien sent you a friend request", sender: "Thien Nguyen"},
+  {type: "misc", subType: "Friend request", content: "Thien sent you a friend request", sender: "This is a really long name for testing an edge case of overflowing text", timeSent: "2021-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Friend request", content: "Dan sent you a friend request", sender: "Daniel Hailu", timeSent: "2021-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Friend request", content: "Tom sent you a friend request", sender: "Tom Bryant", timeSent: "2021-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Nudge", content: "Miguel nudged you", sender: "Miguel Alvarez", timeSent: "2021-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Friend request", content: "Henry sent you a friend request", sender: "Henry Luong", timeSent: "2021-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Friend request", content: "Aaron sent you a friend request", sender: "Aaron Fung", timeSent: "2021-03-10T17:30:01.202Z"},
+  {type: "misc", subType: "Friend request", content: "Thien sent you a friend request", sender: "Thien Nguyen", timeSent: "2021-03-10T17:30:01.202Z"},
 ]
 
 // notification components will go into their own files once prisma schema is done
@@ -30,9 +31,9 @@ function FriendRequestNotif({ notif }: { notif: notification }) {
 
   return (
     <View>
-      <View className="flex flex-row items-center ml-3 mr-3 mt-4 mb-4">
+      <View className="flex flex-row items-center ml-3 mr-3 mt-4">
         {/*profile photo*/}
-          <View className="ml-3 mr-2">
+          <View className="mr-2">
             <MaterialCommunityIcons name="face-man-profile" size={56} color="#CACACA" />
           </View>
         
@@ -40,15 +41,15 @@ function FriendRequestNotif({ notif }: { notif: notification }) {
           {/*sender wants to be your friend*/}
           <Text className="mr-2 mb-2" style={{color: "#CACACA"}}>{notif.sender} wants to be your friend.</Text>
           {/*accept and decline buttons*/}
-          <View className="flex flex-row justify-between mr-2">
-            <TouchableOpacity className="py-2 px-4 rounded-lg flex-1 mr-2" style={{backgroundColor: "#48476D"}}>
+          <View className="flex flex-row justify-between">
+            <TouchableOpacity className="py-2 px-4 rounded-lg flex-1 mr-1" style={{backgroundColor: "#48476D"}}>
               <Text style={{color: "#CACACA"}} className="text-center">Accept</Text>
             </TouchableOpacity>
-            <TouchableOpacity className="py-2 px-4 rounded-lg flex-1 ml-2" style={{backgroundColor: "#CACACA"}}>
+            <TouchableOpacity className="py-2 px-4 rounded-lg flex-1 ml-1" style={{backgroundColor: "#CACACA"}}>
               <Text className="text-center" style={{color: "#1C1B1B"}}>Decline</Text>
             </TouchableOpacity>
           </View>
-          <Text className="text-right pt-2" style={{color: "#CACACA", fontSize: 11}}>{calculateTimeAgo(notif.timeSent)}</Text>
+          <TimeAgo notif={notif} />
         </View>
       </View>
       {/*thin line between notifications*/}
@@ -61,24 +62,22 @@ function NudgeNotif({ notif }: { notif: notification }) {
 
   return (
     <View>
-      <View className="flex flex-row items-center ml-3 mr-3 mt-4 mb-4">
-        {/*profile photo*/}
+      <View className="flex flex-row items-center ml-3 mr-3 mt-4">
+        {/*photo*/}
         <View className="flex flex-col">
-          <View className="ml-3 mr-2">
+          <View className="mr-2">
             <MaterialCommunityIcons name="arm-flex" size={56} color="#CACACA" />
           </View>
         </View>
         
         <View className="flex flex-col flex-1">
-          {/*sender wants to be your friend*/}
+          {/*nudged text*/}
           <Text className="mr-2 mb-2" style={{color: "#CACACA"}}>{notif.sender} nudged you!</Text>
-          {/*accept and decline buttons*/}
-          <View className="flex mr-2">
-            <TouchableOpacity className="py-2 px-4 rounded-lg flex-1 mr-2" style={{backgroundColor: "#CACACA"}}>
-              <Text style={{color: "#1C1B1B"}} className="text-center">Nudge back</Text>
-            </TouchableOpacity>
-          </View>
-          <Text className="text-right pt-2" style={{color: "#CACACA", fontSize: 11}}>{calculateTimeAgo(notif.timeSent)}</Text>
+          {/*nudge back button*/}
+          <TouchableOpacity className="py-2 px-4 rounded-lg flex-1" style={{backgroundColor: "#CACACA"}}>
+            <Text style={{color: "#1C1B1B"}} className="text-center">Nudge back</Text>
+          </TouchableOpacity>
+          <TimeAgo notif={notif} />
         </View>
       </View>
       {/*thin line between notifications*/}
@@ -105,5 +104,9 @@ export default function MiscNotifs() {
     if(notif) renderedNotifications.push(handleNotif(notif, i))
   }
 
-  return renderedNotifications
+  renderedNotifications.push(<View className="pb-10" key={renderedNotifications.length}/>)
+
+  return renderedNotifications.length == 1 ? 
+    <Text className="flex pt-10 text-center" style={{color: "#CACACA"}} key={renderedNotifications.length}> No notifications to display. </Text> : 
+    renderedNotifications
 }
---
+++ apps/expo/src/components/timeAgo/TimeAgo.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+import calculateTimeAgo from "~/utils/calculateTime"
+
+type notification = {type: string, subType: string, content: string, sender?: string, timeSent: string}
+
+export default function TimeAgo({ notif }: { notif: notification }) {
+  const timeAgo = calculateTimeAgo(notif.timeSent)
+  // this is just further error checking/bug prevention. if there is something wrong with the timeAgo calculation then we want 
+  // to just return an empty view component so we don't get any weird "NaN ago" outputs or weird spacing. This can occur if 
+  // the notification object doesn't have a time / if the time zone is wrong and the date is in the future.
+  return timeAgo == "" ? <View className="pt-2 pb-2" /> :
+                         <Text className="text-right pt-2 pb-2" style={{color: "#CACACA", fontSize: 11}}>{timeAgo}</Text>
+}

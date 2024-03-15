407274aa834b7a23210ebcb14599f1b0a2e3a33c
Thien Nguyen <94078395+thienguen@users.noreply.github.com>
2024-03-15 15:05:18 -0700

    33 backend create prisma schema (#77)
    * Chore: Add User and Friend model to prisma schema
    * Add Log, Workout, Exercise, Set, Award, Notification and necessary enums to prisma schema
    * 🦐 feat: you tell me a shrimp fried this rice?
    1. added seed file
    2. seed data procedure
    * 🌻feat: schema now use Int as ID, cause its easier to make mock data this way we can fix, not a biggi 
    * 🌻feat: fixed relation for notif
    * 📚docs: added documentation
    * 🍉backend: mockdata initial, new enum for user status
    * 🍉backend: mockdata PART II, seeded
    * 🥟backend: mockdata my part is donezo now leave it for the other T of my team
    * 🍤backend: ready for review, and UPDATE
    * 🍤backend: sender could be optional, duh due to its coming from system
    * remove email field from user schema. rework notification mock data.
    * fix typo in Collection
    ---------
    Co-authored-by: Marcos Villanueva <mva919@gmail.com>
    Co-authored-by: Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>

---
+++ packages/db/src/seed/seed.ts
@@ -0,0 +1,100 @@
+import { Prisma } from '@prisma/client'
+
+import { prisma } from '..'
+import notification from '../mock-data/notification.json'
+import post from '../mock-data/post.json'
+import users from '../mock-data/user.json'
+
+/**
+ * @param type logging type
+ * @param collection collection name
+ */
+const logger = (type: string, collection: string) => {
+  switch (type) {
+    case 'delete':
+      console.log(`Deleted records in ${collection} collection`)
+      break
+    case 'add':
+      console.log(`Added ${collection} data`)
+      break
+    case 'divider':
+      console.log('----------------------------------')
+      break
+  }
+}
+
+/**
+ * loaddb seed data
+ * Procedure:
+ * 1. Delete all the existing data before?
+ * 2. Add the seed data
+ * @thienguen
+ */
+const loaddb = async () => {
+  try {
+    /// < DELETE PROCEDURE > ///
+
+    await prisma.notification.deleteMany()
+    logger('delete', 'notification')
+
+    await prisma.award.deleteMany()
+    logger('delete', 'award')
+
+    await prisma.set.deleteMany()
+    logger('delete', 'set')
+
+    await prisma.exercise.deleteMany()
+    logger('delete', 'exercise')
+
+    await prisma.workout.deleteMany()
+    logger('delete', 'workout')
+
+    await prisma.log.deleteMany()
+    logger('delete', 'log')
+
+    await prisma.friend.deleteMany()
+    logger('delete', 'friend')
+
+    await prisma.post.deleteMany()
+    logger('delete', 'post')
+
+    await prisma.user.deleteMany()
+    logger('delete', 'user')
+
+    /// < DIVIDER > ///
+    logger('divider', '')
+    logger('divider', '')
+    logger('divider', '')
+    /// < DIVIDER > ///
+
+    /// < SEED PROCEDURE > ///
+    /*  */
+    await prisma.user.createMany({
+      data: users as Prisma.UserCreateManyInput[],
+    })
+    logger('add', 'user')
+
+    /*  */
+    await prisma.post.createMany({ data: post })
+    logger('add', 'post')
+
+    /*  */
+    await prisma.notification.createMany({
+      data: notification as Prisma.NotificationCreateManyInput[],
+    })
+    logger('add', 'notification')
+
+    /*  */
+
+    /*  */
+
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+/* main */
+loaddb().catch((e) => console.error(e))

ef8d980bf2773bdd276613e42c9229fa913719e9
Thien Nguyen <nvktvan@gmail.com>
2024-03-15 17:56:56 -0700

    🐛fix: build III

--- apps/expo/src/app/nav/index.tsx
+++ apps/expo/src/app/nav/index.tsx
@@ -1,35 +1,34 @@
-import type { Route } from 'expo-router'
-import React from 'react'
-import { Button, FlatList, StyleSheet, View } from 'react-native'
-import { router } from 'expo-router'
+import { router } from 'expo-router';
+import React from 'react';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 12,
   },
-})
+});
 
 const Nav = () => {
+
   return (
     <View style={styles.container}>
       <FlatList
         data={[
-          // add your component routes here
-          { key: 'Home', route: '/' },
-          { key: 'Go Auth', route: '/auth' },
-          { key: 'Notif/dm', route: '/notif/dm' },
-          { key: 'Notif/misc', route: '/notif/misc' },
+          { key: 'Home', routeName: 'Home' },
+          { key: 'Go Auth', routeName: 'Auth' },
+          { key: 'Notif/dm', routeName: 'NotifDm' },
+          { key: 'Notif/misc', routeName: 'NotifMisc' }
         ]}
         renderItem={({ item }) => (
           <Button
             title={item.key}
-            onPress={() => router.push(item.route as Route<string>)}
+            onPress={() => router.push(item.routeName)}
           />
         )}
       />
     </View>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;

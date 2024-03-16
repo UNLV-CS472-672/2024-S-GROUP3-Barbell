0363c4e688a015682a6ade85e278bf2ed33f032e
Marcos Villanueva <mva919@gmail.com>
2024-03-15 18:05:58 -0700

    Docs: Add documentation to CustomBottomSheet component

--- apps/expo/src/components/custom-bottom-sheet-modal.tsx
+++ apps/expo/src/components/custom-bottom-sheet-modal.tsx
@@ -14,6 +14,45 @@ export interface CustomBottomSheetModalProps {
   renderBackdrop?: boolean
 }
 
+// This component is a wrapper around the BottomSheetModal component from @gorhom/bottom-sheet.
+// It's used to create a custom bottom sheet modal with the following features:
+// - Custom snap points
+// - Custom start index
+// - Pan down to close
+// - Backdrop
+// Set up the component by passing the following props:
+// - (REQUIRED) children: the content of the modal
+// - (REQUIRED) customSnapPoints: an array of numbers or strings representing the snap points of the modal
+// - (REQUIRED) startIndex: the index of the initial snap point of the customSnapPoints array
+// - enablePanDownToClose: a boolean to enable or disable the pan down to close feature
+// - renderBackdrop: a boolean to enable or disable the backdrop
+// In order to make the modal pop up you will need to make a reference to the component and call the present method.
+// Example:
+/**
+import { useRef } from 'react'
+import { Button, Text, View } from 'react-native'
+import type { CustomBottomSheetModalRef } from '~/components/custom-bottom-sheet-modal'
+import CustomBottomSheetModal from '~/components/custom-bottom-sheet-modal'
+
+const Example = () => {
+  const bottomSheetRef = useRef<CustomBottomSheetModalRef>(null)
+  const handlePresentModalPress = () => bottomSheetRef.current?.present()
+
+  return (
+    <View>
+      <Button title="Present Modal" onPress={handlePresentModalPress}></Button>
+      <CustomBottomSheetModal
+        ref={bottomSheetRef}
+        customSnapPoints={['30%', '93%']}
+        startIndex={1}
+        renderBackdrop
+      >
+        <Text>Example</Text>
+      </CustomBottomSheetModal>
+    </View>
+  )
+}
+*/
 const CustomBottomSheetModal = forwardRef<
   CustomBottomSheetModalRef,
   CustomBottomSheetModalProps

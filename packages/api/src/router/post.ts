25b75b2089c36d23575d76a097b80bafc6dfe9c9
Thien Nguyen <nvktvan@gmail.com>
2024-03-19 00:28:11 -0700

    🤏chores: branch out

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -3,14 +3,24 @@ import { z } from 'zod'
 import { createTRPCRouter, publicProcedure } from '../trpc'
 
 export const postRouter = createTRPCRouter({
+  /**
+   *
+   */
   all: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.post.findMany({ orderBy: { id: 'desc' } })
   }),
+
+  /**
+   *
+   */
   byId: publicProcedure
     .input(z.object({ id: z.number() }))
     .query(({ ctx, input }) => {
       return ctx.prisma.post.findFirst({ where: { id: input.id } })
     }),
+  /**
+   *
+   */
   create: publicProcedure
     .input(
       z.object({
@@ -21,7 +31,10 @@ export const postRouter = createTRPCRouter({
     .mutation(({ ctx, input }) => {
       return ctx.prisma.post.create({ data: input })
     }),
+  /**
+   *
+   */
   delete: publicProcedure.input(z.number()).mutation(({ ctx, input }) => {
-    return ctx.prisma.post.delete({ where: { id: (input) } })
+    return ctx.prisma.post.delete({ where: { id: input } })
   }),
 })

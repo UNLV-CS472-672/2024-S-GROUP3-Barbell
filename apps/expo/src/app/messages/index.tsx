b34efaf7beb1dd812007c2765873c43e72c0e929
Stefano Rubini <stefanorubini@Stefanos-MacBook-Pro.local>
2024-03-22 17:22:26 -0700

    Add new page and new component for messaging.

--- apps/expo/src/app/messages/index.tsx
+++ apps/expo/src/app/messages/index.tsx
@@ -1,11 +1,15 @@
-import { Text, View } from 'react-native'
+import React from 'react'
+import { KeyboardAvoidingView, SafeAreaView, View } from 'react-native'
 
 import MessageInput from '~/components/message/messageInput'
 
 export default function Messages() {
   return (
-    <View className="absolute bottom-5 w-full">
-      <MessageInput />
-    </View>
+    <SafeAreaView style={{ backgroundColor: '#1C1B1B', flex: 1 }}>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <View style={{ flex: 1 }}>{/* Your main content here */}</View>
+        <MessageInput />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
--- apps/expo/src/components/message/messageInput.tsx
+++ apps/expo/src/components/message/messageInput.tsx
@@ -1,19 +1,50 @@
-import { Button, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import {
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import { Octicons } from '@expo/vector-icons'
 
 export default function MessageInput() {
   return (
-    <SafeAreaView>
-      <View className="mx-5 rounded-full bg-gray-500 px-5 py-5">
-        <Text className="text-white">Message...</Text>
-
-        {/* send button */}
-        <View className="absolute right-2 top-2">
-          <View className="bg-dark-purple h-11 w-12 rounded-full">
-            <Button title=">" onPress={() => console.log('clicked')} />
-          </View>
+    <KeyboardAvoidingView style={{ flex: 1, paddingBottom: 10 }}>
+      <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: 10,
+          }}
+        >
+          <TextInput
+            placeholder="Message..."
+            placeholderTextColor="#CACACA"
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              borderColor: '#272727',
+              borderRadius: 20,
+              padding: 10,
+              color: 'white',
+            }}
+            keyboardAppearance="dark"
+          />
+          <TouchableOpacity>
+            <View
+              style={{
+                marginLeft: 10,
+                backgroundColor: '#48476D',
+                borderRadius: 20,
+                padding: 10,
+              }}
+            >
+              <Octicons name="paper-airplane" size={18} color="#CACACA" />
+            </View>
+          </TouchableOpacity>
         </View>
       </View>
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   )
 }

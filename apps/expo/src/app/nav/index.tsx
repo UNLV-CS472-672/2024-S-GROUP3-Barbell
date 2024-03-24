3f51d76491d809fdad89e54d48abe6c2e3f30d1d
AaronFmyHub <146596967+AaronFmyHub@users.noreply.github.com>
2024-03-24 02:04:11 -0700

    Resolve conflict

--- apps/expo/src/app/nav/index.tsx
+++ apps/expo/src/app/nav/index.tsx
@@ -5,30 +5,32 @@ import { router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 12,
-  },
+    container: {
+        flex: 1,
+        paddingTop: 12,
+    },
 })
 
 const Nav = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={[
-          // add your component routes here
-          { key: 'Home', route: '/' },
-          { key: 'Go Auth', route: '/auth' },
-          { key: 'Notif/dm', route: '/notif/dm' },
-          { key: 'Notif/misc', route: '/notif/misc' },
-          { key: 'Award', route: '/award' }
-        ]}
-        renderItem={({ item }) => (
-          <Button title={item.key} onPress={() => router.push(item.route as Route<string>)} />
-        )}
-      />
-    </SafeAreaView>
-  )
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={[
+                    // add your component routes here
+                    { key: 'Home', route: '/' },
+                    { key: 'Go Auth', route: '/auth' },
+                    { key: 'Notif/dm', route: '/notif/dm' },
+                    { key: 'Notif/misc', route: '/notif/misc' },
+                    { key: 'Message Input', route: '/messages'},
+                    { key: 'Create New Workout', route: '/workout/createNew' },
+                    { key: 'Award', route: '/award' },
+                ]}
+                renderItem={({ item }) => (
+                    <Button title={item.key} onPress={() => router.push(item.route as Route<string>)} />
+                )}
+            />
+        </SafeAreaView>
+    )
 }
 
 export default Nav

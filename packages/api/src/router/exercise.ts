8c3e2231dd4f9804b09cd34efaefe11487589fcc
bigboidanwithacan <daniawesomecool@gmail.com>
2024-03-24 20:48:33 -0700

    Created updateExerciseFromExerciseID

--- packages/api/src/router/exercise.ts
+++ packages/api/src/router/exercise.ts
@@ -70,6 +70,58 @@ export const exerciseRouter = createTRPCRouter ({
 
         }),
 
+    /**
+     * This function updates a specific exercise given an id and some field of the exercise
+     *  @params ctx - the context object for this function. It is related to the prisma client used for our database operations.
+     *  @params input - the updated info for the exercise as well as the id of the exercise
+     *  @returns the updated exercise object
+     */
+    updateExerciseFromExericseID: publicProcedure
+        .input(z.object({
+            id: z.number().int(),
+            name: z.string().optional(),
+            note: z.string().optional(),
+            sets: z.array(z.object({
+                id: z.number(),
+                type: z.enum([SetType.DROPSET, SetType.FAILURE, SetType.NORMAL, SetType.WARMUP]),
+                reps: z.number().int(),
+                weight: z.number(),
+                exerciseId: z.number().int()
+            })).optional(),
+            body_part: z.enum([ BodyPart.LEGS, BodyPart.ARMS, BodyPart.CHEST, BodyPart.BACK, BodyPart.SHOULDERS, BodyPart.CORE, BodyPart.FULL_BODY, BodyPart.OTHER]).optional(),
+            category: z.enum([Category.BARBELL, Category.DUMBBELL, Category.MACHINE, Category.ASSISTED_BODYWEIGHT, Category.WEIGHTED_BODYWEIGHT
+                            , Category.BODYWEIGHT, Category.DURATION, Category.CARDIO, Category.REPS_ONLY, Category.OTHER]).optional(),
+            workoutId: z.number().int().optional()
+        }))
+        .mutation(async({ctx, input})=>{
+            const { prisma } = ctx
+
+            return prisma.exercise.update({
+                where: {
+                    id: input.id
+                },
+                data:{
+                    name: input.name,
+                    note: input.note,
+                    sets: {
+                        updateMany: input.sets?.map((set)=> ({
+                            where: {
+                                id: set.id
+                            },
+                            data:{
+                                type: set.type,
+                                reps: set.reps,
+                                weight: set.weight
+                            }
+                        })
+                    )
+                    },
+                    body_part: input.body_part,
+                    category: input.category,
+                    workoutId: input.workoutId,
+                }
+            })
+        })
 
 })
 

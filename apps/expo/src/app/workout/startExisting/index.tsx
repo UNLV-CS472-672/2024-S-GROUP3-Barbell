ee9c9e7dc94fbd42013a234e508d057f23160203
Henry <henryluong19@gmail.com>
2024-03-26 16:09:08 -0700

    Update index.tsx

--- apps/expo/src/app/workout/startExisting/index.tsx
+++ apps/expo/src/app/workout/startExisting/index.tsx
@@ -6,7 +6,6 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 5,
     backgroundColor: '#1E1E1E',
   },
 })

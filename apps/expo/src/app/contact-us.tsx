a5550a9e16b8001c71dc324b91e4ca2c8be40a64
Elliot Wesoff <ewesoff@gmail.com>
2024-03-27 02:50:55 +0000

    43 frontend profile screen structure (#116)
    * working on implementing profile screen layout. wip
    
    * some profile refactors, add icons, better 'page not found' screen.
    
    * create screens navigateable from user profile screen.
    
    * fix user profile styles, move some details to personal data screen, fix VirtualizedList errors.

---
+++ apps/expo/src/app/contact-us.tsx
@@ -0,0 +1,15 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const ContactUs = () => {
+  return (
+    <SafeAreaView>
+      <Text style={ { textAlign: 'center', padding: 30 }}>
+        don't contact us
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default ContactUs;
+

823119f3d628ed61ea6dd8df35fe357f866b5c85
Thien Nguyen <nvktvan@gmail.com>
2024-03-29 05:15:20 -0700

    ✅build: final touch of files location and for better standard

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 
+/**
+ * @thienguen
+ * since we installed it at root
+ * so it shared instances across all monorepo, quite hectic
+ */
 export default defineConfig({
   test: {
     // ...

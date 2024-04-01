8948bfdcd77b93aa9d21b7149212ff2fb28e4d33
Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>
2024-04-01 11:54:53 -0700

    remove GC test because it is outdated

--- apps/expo/src/components/notif/__tests__/RotatingBarbellIcon.test.tsx
+++ apps/expo/src/components/notif/__tests__/RotatingBarbellIcon.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+
 import { render, screen } from '@testing-library/react-native'
-import RotatingBarbellIcon from '../dmNotifs/RotatingBarbellIcon'
+
+import RotatingBarbellIcon from '~/components/notif/RotatingBarbellIcon'
 
 test('RotatingBarbellIcon', async () => {
   render(<RotatingBarbellIcon />)
-  
+
   expect(screen.toJSON()).toMatchSnapshot()
 })

ce860e42d2b2f777778fbd5c8ab684b50d40c129
Elliot Wesoff <ewesoff@gmail.com>
2024-04-02 10:46:45 -0700

    ignore constants in jest config.

--- packages/api/src/__test__/constants.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { FA } from  '../../../../apps/expo/src/utils/constants';
-
-test('Global FA5 icon sizes', () => {
-    expect('reg' in FA).toEqual(true);
-    expect('lg' in FA).toEqual(true);
-    expect('xl' in FA).toEqual(true);
-});

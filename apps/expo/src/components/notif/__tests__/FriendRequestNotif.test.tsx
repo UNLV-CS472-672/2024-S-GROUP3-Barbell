f9597a8813c2e8842ce2cfca047af6ab2b01ec8d
Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>
2024-04-07 16:20:52 -0700

    modify test with new field

--- apps/expo/src/components/notif/__tests__/FriendRequestNotif.test.tsx
+++ apps/expo/src/components/notif/__tests__/FriendRequestNotif.test.tsx
@@ -1,24 +1,33 @@
 import React from 'react'
-import { fireEvent, render, screen } from '@testing-library/react-native'
-import FriendRequestNotif from '~/components/notif/miscNotifs/FriendRequestNotif'
+
 import { Notification } from '@prisma/client'
+import { fireEvent, render, screen } from '@testing-library/react-native'
+
+import FriendRequestNotif from '~/components/notif/miscNotifs/FriendRequestNotif'
 
 test('FriendRequestNotif', async () => {
   const expectedNotif: Notification = {
-    "id": 3,
-    "createdAt": new Date("2024-03-14T10:15:00Z"),
-    "content": "wants to be your friend",
-    "type": "FRIEND_REQUEST",
-    "read": false,
-    "receiverId": 4,
-    "senderId": 2
+    id: 3,
+    createdAt: new Date('2024-03-14T10:15:00Z'),
+    content: 'wants to be your friend',
+    type: 'FRIEND_REQUEST',
+    read: false,
+    receiverId: 4,
+    senderId: 2,
   }
   const expectedSenderUsername = 'username'
   const expectedContent = expectedNotif.content
+  const expectedReceiverId = expectedNotif.receiverId
 
-  render(<FriendRequestNotif notif={expectedNotif} senderUsername={expectedSenderUsername} />)
+  render(
+    <FriendRequestNotif
+      notif={expectedNotif}
+      senderUsername={expectedSenderUsername}
+      receiverId={expectedReceiverId}
+    />,
+  )
   const senderUsernameOutput = await screen.findByTestId('sender-username-with-content')
-  
+
   expect(senderUsernameOutput).toHaveTextContent(expectedSenderUsername + ' ' + expectedContent)
   expect(screen.toJSON()).toMatchSnapshot()
 })

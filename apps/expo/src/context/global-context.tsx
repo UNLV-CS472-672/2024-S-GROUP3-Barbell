1c8ebceb541ea838c0e65d28fffdeb5a29cc85d4
Thien Nguyen <nvktvan@gmail.com>
2024-04-06 23:04:08 -0700

    🐞fixed: development, thanks for catching this stef

--- apps/expo/src/context/global-context.tsx
+++ apps/expo/src/context/global-context.tsx
@@ -65,7 +65,7 @@ const GlobalContextProvider = ({ children }: IGlobalContextProviderProps) => {
         })
       }
     }
-  }, [clerkUserData, createUser])
+  }, [clerkUserData])
 
   useEffect(() => {
     getUserData()

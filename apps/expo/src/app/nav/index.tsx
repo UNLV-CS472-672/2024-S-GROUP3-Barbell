b6118a1ec2ebc32dc1cc12520724a0d3283c0478
bigboidanwithacan <daniawesomecool@gmail.com>
2024-04-07 03:02:09 -0700

    temporary push

--- apps/expo/src/app/nav/index.tsx
+++ apps/expo/src/app/nav/index.tsx
@@ -34,6 +34,7 @@ const Nav = () => {
           { key: 'Award', route: '/award' },
           { key: 'Spotify', route: '/spotify' },
           { key: 'Tracker', route: '/tracker' },
+          { key: 'Test', route: '/test'},
         ]}
         renderItem={({ item }) => (
           <Button
---
+++ apps/expo/src/app/test/index.tsx
@@ -0,0 +1,49 @@
+import { api } from '~/utils/api'
+import { SafeAreaView, Button } from 'react-native'
+import { BodyPart } from '@prisma/client'
+import { Category } from '@prisma/client'
+
+
+const Test: React.FC = () =>{
+
+    const { mutate,error } = api.workout.createNewWorkout.useMutation({
+        async onSuccess() {
+            console.log('Exercise created')
+        },
+        async onError(){
+            console.log(error)
+        }
+    })
+
+    const createWorkout = async () => {
+        mutate({
+            name: 'test workout',
+            duration: 340,
+            finishedAt: new Date(),
+            exercises: [{
+                id: 1
+            },
+            {
+                id: 2
+            },
+            {
+                id: 3
+            },
+            {
+                id: 4
+            },
+            {
+                id: 5
+            }
+        ],
+            userId: 1
+        })
+    }
+
+    return <SafeAreaView>
+        <Button title="Create Workout" onPress={createWorkout}/>
+    </SafeAreaView>
+
+}
+
+export default Test

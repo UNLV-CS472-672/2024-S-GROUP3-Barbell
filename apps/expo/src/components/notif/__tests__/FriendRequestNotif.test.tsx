5135eaebb7aa0aab1f19fc63f250663b631f37f7
mva919 <mva919@gmail.com>
2024-04-07 16:47:30 -0700

    Fix this goofy test

--- apps/expo/src/components/notif/__tests__/FriendRequestNotif.test.tsx
+++ apps/expo/src/components/notif/__tests__/FriendRequestNotif.test.tsx
@@ -1,33 +1,55 @@
 import React from 'react'
 
 import { Notification } from '@prisma/client'
-import { fireEvent, render, screen } from '@testing-library/react-native'
+import { render, screen } from '@testing-library/react-native'
 
 import FriendRequestNotif from '~/components/notif/miscNotifs/FriendRequestNotif'
 
-test('FriendRequestNotif', async () => {
-  const expectedNotif: Notification = {
-    id: 3,
-    createdAt: new Date('2024-03-14T10:15:00Z'),
-    content: 'wants to be your friend',
-    type: 'FRIEND_REQUEST',
-    read: false,
-    receiverId: 4,
-    senderId: 2,
-  }
-  const expectedSenderUsername = 'username'
-  const expectedContent = expectedNotif.content
-  const expectedReceiverId = expectedNotif.receiverId
+jest.mock('~/utils/api', () => ({
+  __esModule: true,
+  api: {
+    useUtils: jest.fn(() => ({
+      notif: {
+        getMiscNotifsWithSenderUsernameFromUserId: {
+          invalidate: jest.fn(),
+        },
+      },
+    })),
+    friend: {
+      makeFriendsReceiverIdSenderId: {
+        useMutation: jest.fn(() => ({
+          mutateAsync: jest.fn(),
+        })),
+      },
+    },
+  },
+}))
 
-  render(
-    <FriendRequestNotif
-      notif={expectedNotif}
-      senderUsername={expectedSenderUsername}
-      receiverId={expectedReceiverId}
-    />,
-  )
-  const senderUsernameOutput = await screen.findByTestId('sender-username-with-content')
+describe('FriendRequestNotif', () => {
+  it("should render sender's username and content", async () => {
+    const expectedNotif: Notification = {
+      id: 3,
+      createdAt: new Date('2024-03-14T10:15:00Z'),
+      content: 'wants to be your friend',
+      type: 'FRIEND_REQUEST',
+      read: false,
+      receiverId: 4,
+      senderId: 2,
+    }
+    const expectedSenderUsername = 'username'
+    const expectedContent = expectedNotif.content
+    const expectedReceiverId = expectedNotif.receiverId
 
-  expect(senderUsernameOutput).toHaveTextContent(expectedSenderUsername + ' ' + expectedContent)
-  expect(screen.toJSON()).toMatchSnapshot()
+    render(
+      <FriendRequestNotif
+        notif={expectedNotif}
+        senderUsername={expectedSenderUsername}
+        receiverId={expectedReceiverId}
+      />,
+    )
+    const senderUsernameOutput = await screen.findByTestId('sender-username-with-content')
+
+    expect(senderUsernameOutput).toHaveTextContent(expectedSenderUsername + ' ' + expectedContent)
+    expect(screen.toJSON()).toMatchSnapshot()
+  })
 })

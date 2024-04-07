96858e0dfe4d15a5be862584dd983387a1e495e9
Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>
2024-04-07 01:44:49 -0700

    gotta merge. ah jeez

--- packages/api/src/router/friend.ts
+++ packages/api/src/router/friend.ts
@@ -1,5 +1,6 @@
-import { z } from 'zod';
-import { createTRPCRouter, publicProcedure } from '../trpc';
+import { z } from 'zod'
+
+import { createTRPCRouter, publicProcedure } from '../trpc'
 
 export const friendRouter = createTRPCRouter({
   /**
@@ -9,67 +10,68 @@ export const friendRouter = createTRPCRouter({
     return ctx.prisma.friend.findMany({
       orderBy: { id: 'asc' },
       include: { user: true },
-    });
+    })
   }),
 
   /**
    * Get friend by id
    */
-  byId: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .query(({ ctx, input }) => {
-      return ctx.prisma.friend.findFirst({
-        where: { id: input.id },
-        include: { user: true },
-      });
-    }),
+  byId: publicProcedure.input(z.object({ id: z.number() })).query(({ ctx, input }) => {
+    return ctx.prisma.friend.findFirst({
+      where: { id: input.id },
+      include: { user: true },
+    })
+  }),
 
   /**
-   * Create a friend
+   *  @remarks
+   *  This accepts or denies a friend request for the user.
+   *
+   *  @param  receiverId - the id of the one who receives, logged in user
+   *  @param  senderId - the id of the one who sends, *not* logged in user
+   *  @returns the accept or deny mutation, adds as friend or does not add
    */
-  create: publicProcedure
+  makeFriendsReceiverIdSenderId: publicProcedure
     .input(
       z.object({
-        userId: z.number(),
-      })
+        receiverId: z.number().int(),
+        senderId: z.number().int(),
+      }),
     )
     .mutation(({ ctx, input }) => {
-      return ctx.prisma.friend.create({
+      ctx.prisma.friend.create({
         data: {
-          user: { connect: { id: input.userId } },
-        },
-        include: { user: true },
-      });
+          friendId: 
+        }
+      })
     }),
 
   /**
    * Delete a friend
    */
-  delete: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .mutation(({ ctx, input }) => {
-      return ctx.prisma.friend.delete({
-        where: { id: input.id },
-      });
-    }),
+  delete: publicProcedure.input(z.object({ id: z.number() })).mutation(({ ctx, input }) => {
+    return ctx.prisma.friend.delete({
+      where: { id: input.id },
+    })
+  }),
 
   /**
    * Update a friend: UNUSED, but here for reference
    */
-//   update: publicProcedure
-//     .input(
-//       z.object({
-//         id: z.number(),
-//         userId: z.number().optional(),
-//       })
-//     )
-//     .mutation(({ ctx, input }) => {
-//       return ctx.prisma.friend.update({
-//         where: { id: input.id },
-//         data: {
-//           user: input.userId ? { connect: { id: input.userId } } : undefined,
-//         },
-//         include: { user: true },
-//       });
-//     }),
-});
+  //   update: publicProcedure
+  //     .input(
+  //       z.object({
+  //         id: z.number(),
+  //         userId: z.number().optional(),
+  //       })
+  //     )
+  //     .mutation(({ ctx, input }) => {
+  //       return ctx.prisma.friend.update({
+  //         where: { id: input.id },
+  //         data: {
+  //           user: input.userId ? { connect: { id: input.userId } } : undefined,
+  //         },
+  //         include: { user: true },
+  //       });
+  //     }),
+})

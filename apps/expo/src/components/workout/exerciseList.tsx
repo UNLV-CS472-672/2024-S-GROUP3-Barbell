65aa301190cf3a79f85fc7aadf36476a9576c47e
Taylor Finelli <taylorfinelli@Taylors-MacBook-Pro.local>
2024-04-07 23:15:50 -0700

    Simplify ExerciseList return logic

--- apps/expo/src/components/workout/exerciseList.tsx
+++ apps/expo/src/components/workout/exerciseList.tsx
@@ -1,28 +1,19 @@
-import { api } from '~/utils/api'
-import { Text, View } from 'react-native'
-import RotatingBarbellIcon from '~/components/notif/RotatingBarbellIcon'
 import React from 'react'
+import { Text, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 
+import RotatingBarbellIcon from '~/components/notif/RotatingBarbellIcon'
+import { api } from '~/utils/api'
+
 export default function ExerciseList() {
-    const { data, isFetched, isFetching } = api.exercise.getAllExercises.useQuery()
-    
-    console.log(data)
-    
-    if (isFetching) {
-        return <RotatingBarbellIcon />
-    }
+  const { data, isFetched, isFetching } = api.exercise.getAllExercises.useQuery()
 
-    if (isFetched || data === undefined) {
-        return <ScrollView><Text>HELP</Text></ScrollView>
-    }
+  const exercises = <ScrollView>{data?.map((exercise) => <Text key={exercise.id}>{exercise.name}</Text>)}</ScrollView>
 
-    if(data != undefined)
-    return(
-        <View>
-            {data.map((exercise) => (
-                <Text key={exercise.id}>{exercise.name}</Text>
-            ))}
-        </View>
-    )
+  return (
+    <View>
+      {isFetching && <RotatingBarbellIcon />}
+      {isFetched && exercises}
+    </View>
+  )
 }

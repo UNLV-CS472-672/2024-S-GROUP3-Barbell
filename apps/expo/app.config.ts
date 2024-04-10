1b152bb72c529c0a184b2298f1c2e7844c22b27a
Thien Nguyen <nvktvan@gmail.com>
2024-04-10 01:25:34 -0700

    Auto stash before merge of "165-frontend-move-header-to-the-correct-location" and "origin/165-frontend-move-header-to-the-correct-location"

---
+++ apps/expo/app.config.ts
@@ -0,0 +1,59 @@
+import type { ExpoConfig } from 'expo/config'
+
+const defineConfig = (): ExpoConfig => ({
+  name: 'expo',
+  slug: 'expo',
+  scheme: 'expo',
+  version: '0.1.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#1F104A',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+    url: 'https://u.expo.dev/a5b98934-bf53-4573-ba91-972c22a6759a',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    bundleIdentifier: 'your.bundle.identifier',
+    supportsTablet: true,
+  },
+  android: {
+    package: 'your.bundle.identifier',
+    adaptiveIcon: {
+      foregroundImage: './assets/icon.png',
+      backgroundColor: '#1F104A',
+    },
+    jsEngine: 'hermes',
+  },
+  extra: {
+    // this is commented out so you don't have to log in to run expo start locally
+    eas: {
+      projectId: 'a5b98934-bf53-4573-ba91-972c22a6759a',
+    },
+    clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    expoPublicClerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+  // jsEngine: 'hermes',
+  plugins: [
+    /* don't touch this */
+    // 'expo-router',
+    ["expo-router", { "root": "./src/app/" }],
+    'expo-font',
+    'expo-secure-store',
+    // './expo-plugins/with-modify-gradle.js',
+  ],
+})
+
+export default defineConfig

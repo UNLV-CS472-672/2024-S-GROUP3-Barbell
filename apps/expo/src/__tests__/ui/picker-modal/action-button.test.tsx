683067dbbad3a26507f9414e793e33dff1a480c3
thienguen <nvktvan@gmail.com>
2024-04-10 23:39:23 -0700

    🐧ci/cd: passed action-button, now onto picker-modal

--- apps/expo/src/__tests__/ui/picker-modal/action-button.test.tsx
+++ apps/expo/src/__tests__/ui/picker-modal/action-button.test.tsx
@@ -1,24 +1,42 @@
 import React from 'react'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
 import { fireEvent, render } from '@testing-library/react-native'
 
 import ActionButton from '~/components/ui/picker-modal/action-button/action-button'
 
+interface MockTouchableProps extends TouchableOpacityProps {
+  testID?: string
+}
+
+const MockTouchable: React.FC<MockTouchableProps> = ({ children, onPress, testID, ...props }) => (
+  <TouchableOpacity onPress={onPress} testID={testID} {...props}>
+    {children}
+  </TouchableOpacity>
+)
+
 describe('ActionButton', () => {
   it('renders correctly with text', () => {
-    const { getByText } = render(<ActionButton text='Press Me' />)
-    expect(getByText('Press Me')).toBeTruthy()
+    const { getByText } = render(
+      <ActionButton text='thienguen' TouchableComponent={MockTouchable} />,
+    )
+    expect(getByText('thienguen')).toBeTruthy()
   })
 
   it('handles press events', () => {
     const onPressMock = jest.fn()
-    const { getByTestId } = render(<ActionButton onActionPress={onPressMock} />)
+    const { getByTestId } = render(
+      <ActionButton onActionPress={onPressMock} TouchableComponent={MockTouchable} />,
+    )
     fireEvent.press(getByTestId('action-button'))
     expect(onPressMock).toHaveBeenCalled()
   })
 
   it('conditionally renders the Divider', () => {
-    const { queryByTestId } = render(<ActionButton isLastItem={true} />)
+    const { queryByTestId } = render(
+      <ActionButton isLastItem={true} TouchableComponent={MockTouchable} />,
+    )
+    // Since the `queryByTestId` for 'divider' would not work because the Divider itself does not accept testID as a prop,
     expect(queryByTestId('divider')).toBeNull()
   })
 })

dc5f34b237ed0e4daee2ed8c89d519288f502e43
Taylor Finelli <taylorfinelli25@gmail.com>
2024-04-14 19:54:36 -0700

    Unit tests for keypad.tsx

--- apps/expo/src/components/ui/keypad/Keypad.tsx
+++ apps/expo/src/components/ui/keypad/Keypad.tsx
@@ -3,21 +3,6 @@ import { Dimensions, Text, TouchableOpacity, View } from 'react-native'
 
 import { AntDesign, Ionicons } from '@expo/vector-icons'
 
-interface KeyProps {
-  number: string
-  handleKeyPress: (value: string) => void
-}
-
-function Key({ number, handleKeyPress } : KeyProps){
-  return (
-    <TouchableOpacity className="m-1 flex-1 justify-center rounded-md bg-white" onPress={() => handleKeyPress(number)}>
-      <Text style={{ fontSize: 24 }} className="text-center">
-        {number}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
 interface NumericKeypadProps {
   setNumber: React.Dispatch<React.SetStateAction<any>>
   number: string
@@ -25,10 +10,15 @@ interface NumericKeypadProps {
   setKeypadVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export default function Keypad({ setNumber, number, keypadVisible, setKeypadVisible }: NumericKeypadProps) {
+export default function Keypad({
+  setNumber,
+  number,
+  keypadVisible,
+  setKeypadVisible,
+}: NumericKeypadProps) {
   const handleKeyPress = (value: string) => {
     if (value === 'backspace') {
-      setNumber(number.slice(0, -1)) // else just remove the last digit
+      setNumber(number.slice(0, -1)) // remove the last digit
     } else if (value === '.') {
       if (!number.includes('.')) {
         // only add a '.' if there is not one already
@@ -55,38 +45,131 @@ export default function Keypad({ setNumber, number, keypadVisible, setKeypadVisi
   return !keypadVisible ? (
     <View />
   ) : (
-    <View className="flex bg-slate-200 p-1" style={{ height: height * 0.35 }}>
-      <View className="flex-1 flex-row justify-between">
-        <Key number='1' handleKeyPress={handleKeyPress}/>
-        <Key number='2' handleKeyPress={handleKeyPress}/>
-        <Key number='3' handleKeyPress={handleKeyPress}/>
-      </View>
-      <View className="flex-1 flex-row justify-between">
-        <Key number='4' handleKeyPress={handleKeyPress}/>
-        <Key number='5' handleKeyPress={handleKeyPress}/>
-        <Key number='6' handleKeyPress={handleKeyPress}/>
-      </View>
-      <View className="flex-1 flex-row justify-between">
-        <Key number='7' handleKeyPress={handleKeyPress}/>
-        <Key number='8' handleKeyPress={handleKeyPress}/>
-        <Key number='9' handleKeyPress={handleKeyPress}/>
-      </View>
-      <View className="flex-1 flex-row justify-between">
-        <Key number='.' handleKeyPress={handleKeyPress}/>
-        <Key number='0' handleKeyPress={handleKeyPress}/>
+    <View className='flex bg-slate-200 p-1' style={{ height: height * 0.35 }}>
+      <View className='flex-1 flex-row justify-between'>
         <TouchableOpacity
-          className="m-1 flex-1 items-center justify-center rounded-md bg-white"
+          testID='test-1'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('1')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            1
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-2'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('2')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            2
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-3'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('3')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            3
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className='flex-1 flex-row justify-between'>
+        <TouchableOpacity
+          testID='test-4'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('4')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            4
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-5'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('5')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            5
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-6'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('6')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            6
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className='flex-1 flex-row justify-between'>
+        <TouchableOpacity
+          testID='test-7'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('7')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            7
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-8'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('8')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            8
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-9'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('9')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            9
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View className='flex-1 flex-row justify-between'>
+        <TouchableOpacity
+          testID='test-.'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('.')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            .
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-0'
+          className='m-1 flex-1 justify-center rounded-md bg-white'
+          onPress={() => handleKeyPress('0')}
+        >
+          <Text style={{ fontSize: 24 }} className='text-center'>
+            0
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          testID='test-backspace'
+          className='m-1 flex-1 items-center justify-center rounded-md bg-white'
           onPress={() => handleKeyPress('backspace')}
         >
-          <Ionicons name="backspace-outline" size={24} color="black" />
+          <Ionicons name='backspace-outline' size={24} color='black' />
         </TouchableOpacity>
       </View>
-      <View className="h-12 flex-row">
-        <TouchableOpacity onPress={() => setKeypadVisible(false)} className="flex-1 items-center justify-center">
-          <AntDesign name="down" size={18} color="black" />
+      <View className='h-12 flex-row'>
+        <TouchableOpacity
+          testID='test-minimize'
+          onPress={() => setKeypadVisible(false)}
+          className='flex-1 items-center justify-center'
+        >
+          <AntDesign name='down' size={18} color='black' />
         </TouchableOpacity>
-        <View className="flex-1" />
-        <View className="flex-1" />
+        <View className='flex-1' />
+        <View className='flex-1' />
       </View>
     </View>
   )

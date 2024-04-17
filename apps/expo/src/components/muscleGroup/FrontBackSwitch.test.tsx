57665c9b0c74821a0c3049a619b9ce1def7ac0d0
AaronFmyHub <146596967+AaronFmyHub@users.noreply.github.com>
2024-04-16 23:47:50 -0700

    Update tests

--- apps/expo/src/components/muscleGroup/FrontBackSwitch.test.tsx
+++ apps/expo/src/components/muscleGroup/FrontBackSwitch.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react-native'
 
 import FrontBackSwitch from "~/components/muscleGroup/FrontBackSwitch";
+import {useState} from "react";
 
 test('FrontBackSwitch', () => {
-  render(<FrontBackSwitch />)
+  const [tmp, setTmp] = useState(false)
+  const tmpCh = () => setTmp((previousState) => !previousState)
+
+  render(<FrontBackSwitch
+    onValueChange={tmpCh}
+    value={tmp}
+    label={""}
+  />)
   expect(screen.toJSON()).toMatchSnapshot()
 })
--- apps/expo/src/components/toggle/Toggle.test.tsx
+++ apps/expo/src/components/toggle/Toggle.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react-native'
-
 import Toggle from './Toggle'
+import {useState} from "react";
 
 test('Toggle', () => {
-  render(<Toggle />)
+  const [tmp, setTmp] = useState(false)
+  const tmpCh = () => setTmp((previousState) => !previousState)
+  render(<Toggle
+    onValueChange={tmpCh}
+    value={tmp}
+    label={""}
+  />)
   expect(screen.toJSON()).toMatchSnapshot()
 })

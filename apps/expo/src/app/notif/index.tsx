e3618edd823d506081b5bd9917261a8124125fb0
Taylor Finelli <taylorfinelli25@gmail.com>
2024-04-17 16:04:10 -0700

    New nav bar + implementation into notif index.tsx

--- apps/expo/src/app/notif/index.tsx
+++ apps/expo/src/app/notif/index.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { router } from 'expo-router'
 
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import DmNotifs from '~/components/notif/dmNotifs/dmNotifs'
 import GcNotifs from '~/components/notif/gcNotifs/gcNotifs'
 import MiscNotifs from '~/components/notif/miscNotifs/miscNotifs'
+import NavBar from '~/components/ui/nav-bar/NavBar'
 
 export type notifsType = 'misc' | 'dm' | 'gc'
 
@@ -18,40 +18,51 @@ export default function NotifScreen() {
   return (
     <SafeAreaView style={{ backgroundColor: '#1C1B1B', flex: 1 }}>
       {/*header w/ back and new message buttons*/}
-      <View className="flex flex-row justify-between px-5">
-        <Ionicons onPress={() => router.back()} name="chevron-back" size={24} color="#CACACA" />
-        <Text style={{ color: '#CACACA', fontSize: 20 }}>Notifications</Text>
-        <MaterialCommunityIcons name="message-plus-outline" size={24} color="#CACACA" />
-      </View>
+      <NavBar
+        center={'Notifications'}
+        right={<MaterialCommunityIcons name='message-plus-outline' size={24} color='#CACACA' />}
+      />
 
       {/*notif nav buttons*/}
-      <View className="m-2 flex flex-row items-center">
+      <View className='m-2 flex flex-row items-center'>
         <TouchableOpacity
-          className="ml-1 mr-1 mt-1 flex-1 rounded-lg px-4 py-2 font-bold"
+          className='ml-1 mr-1 mt-1 flex-1 rounded-lg px-4 py-2 font-bold'
           style={{ backgroundColor: visibleNotifs == 'misc' ? '#48476D' : '#CACACA' }}
           onPress={() => {
             setVisibleNotifs('misc')
           }}
         >
-          <Text style={{ color: visibleNotifs == 'misc' ? '#CACACA' : '#1C1B1B', textAlign: 'center' }}>General</Text>
+          <Text
+            style={{ color: visibleNotifs == 'misc' ? '#CACACA' : '#1C1B1B', textAlign: 'center' }}
+          >
+            General
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          className="ml-1 mr-1 mt-1 flex-1 rounded-lg px-4 py-2 font-bold"
+          className='ml-1 mr-1 mt-1 flex-1 rounded-lg px-4 py-2 font-bold'
           style={{ backgroundColor: visibleNotifs == 'dm' ? '#48476D' : '#CACACA' }}
           onPress={() => {
             setVisibleNotifs('dm')
           }}
         >
-          <Text style={{ color: visibleNotifs == 'dm' ? '#CACACA' : '#1C1B1B', textAlign: 'center' }}>Direct</Text>
+          <Text
+            style={{ color: visibleNotifs == 'dm' ? '#CACACA' : '#1C1B1B', textAlign: 'center' }}
+          >
+            Direct
+          </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          className="ml-1 mr-1 mt-1 flex-1 rounded-lg px-4 py-2 font-bold"
+          className='ml-1 mr-1 mt-1 flex-1 rounded-lg px-4 py-2 font-bold'
           style={{ backgroundColor: visibleNotifs == 'gc' ? '#48476D' : '#CACACA' }}
           onPress={() => {
             setVisibleNotifs('gc')
           }}
         >
-          <Text style={{ color: visibleNotifs == 'gc' ? '#CACACA' : '#1C1B1B', textAlign: 'center' }}>Group</Text>
+          <Text
+            style={{ color: visibleNotifs == 'gc' ? '#CACACA' : '#1C1B1B', textAlign: 'center' }}
+          >
+            Group
+          </Text>
         </TouchableOpacity>
       </View>
 
@@ -59,7 +70,7 @@ export default function NotifScreen() {
       <View style={{ borderBottomWidth: 1, borderBottomColor: '#737272' }} />
 
       {/*list of notifications*/}
-      <ScrollView className="flex-1">
+      <ScrollView className='flex-1'>
         <View>
           {visibleNotifs == 'misc' && <MiscNotifs />}
           {visibleNotifs == 'dm' && <DmNotifs />}

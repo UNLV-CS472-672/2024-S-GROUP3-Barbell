43f39bd188a17e6eb7112839122c9541ec3d15fa
AaronFmyHub <146596967+AaronFmyHub@users.noreply.github.com>
2024-04-18 01:26:21 -0700

    Add gender switch test

--- apps/expo/src/components/muscleGroup/__snapshots__/GenderSwitch.test.tsx
+++ apps/expo/src/components/muscleGroup/__snapshots__/GenderSwitch.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from '@testing-library/react-native'
 import Toggle from '~/components/toggle/toggle'
+import GenderSwitch from '~/components/muscleGroup/GenderSwitch'
 
-describe('Toggle', () => {
+describe('GenderSwitch', () => {
   it('should match snapshot', () => {
     const { toJSON } = render(
-      <Toggle onValueChange={() => {}} value={false} label='test' key='stuff' />,
+      <GenderSwitch onValueChange={() => {}} value={false} key='stuff' />,
     )
     expect(toJSON()).toMatchSnapshot()
   })

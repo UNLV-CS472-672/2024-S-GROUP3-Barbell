985feb09668f2713d434a33f18dd3782f5004bde
bigboidanwithacan <38168108+bigboidanwithacan@users.noreply.github.com>
2024-04-19 19:35:08 -0700

    starting test

---
+++ apps/expo/src/app/privacy-policy/privacy-policy.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { render, screen } from '@testing-library/react-native'
+
+import PrivacyPolicy from '.'
+
+test('renders policy page and checks text content', () => {
+  render(<PrivacyPolicy />)
+
+  
+})

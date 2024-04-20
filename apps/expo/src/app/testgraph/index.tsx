5187975741db1a639fd24db2e9e0d85dce5f89df
Miguel <malvrez0525@gmail.com>
2024-04-20 16:37:02 -0700

    Slight modification to index.tsx. Just commented out API testing.

--- apps/expo/src/app/testgraph/index.tsx
+++ apps/expo/src/app/testgraph/index.tsx
@@ -12,44 +12,42 @@ import { api } from '~/utils/trpc/api';
 
 
 export default function GraphScreen(){
-    // Just testing out the new WorkoutLog api
-    const {singleLog} = api.workoutLog.getWorkoutLogByID.useQuery({id: 1});
-    // Testing multiple logs
-    const {multipleLogs} = api.workoutLog.getWorkoutLogsByUserID.useQuery({userId: 1})
+    // // Just testing out the new WorkoutLog api
+    // const {singleLog} = api.workoutLog.getWorkoutLogByID.useQuery({id: 1});
+    // // Testing multiple logs
+    // const {multipleLogs} = api.workoutLog.getWorkoutLogsByUserID.useQuery({userId: 1})
 
-    // Creating test workoutLog
-    const {mutate, error } = api.workoutLog.createNewWorkoutLog.useMutation({
-        async onSuccess(){
-            console.log("New workout log created")
-        },
-        async onError(){
-            console.log(error)
-        }
-    })
-    const createWorkoutLog = async() => {
-        mutate({
-            duration: 0,
-            userId: 5,
-            workoutTemplateId: 1
-        })
-    }
+    // // Creating test workoutLog
+    // const {mutate, error } = api.workoutLog.createNewWorkoutLog.useMutation({
+    //     async onSuccess(){
+    //         console.log("New workout log created")
+    //     },
+    //     async onError(){
+    //         console.log(error)
+    //     }
+    // })
+    // const createWorkoutLog = async() => {
+    //     mutate({
+    //         duration: 0,
+    //         userId: 5,
+    //         workoutTemplateId: 1
+    //     })
+    // }
 
-    const updateLog = api.workoutLog.updateWorkoutLog.useMutation()
-    const handleUpdate = () => {
-        updateLog.mutate({
-            id: 19,
-            finishedAt: new Date(),
-            duration: 340,
-        })
-    }
-    
+    // const updateLog = api.workoutLog.updateWorkoutLog.useMutation()
+    // const handleUpdate = () => {
+    //     updateLog.mutate({
+    //         id: 19,
+    //         finishedAt: new Date(),
+    //         duration: 340,
+    //     })
+    // }
 
+    // Actual stuff now. Just invoke the graph thingy.
     return(
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Testing testing</Text>
             <WorkoutFrequencyGraph userid={2}/>
-            <Button title="Create new log" onPress={createWorkoutLog}/>
-            <Button title="Update Log" onPress={handleUpdate}/>
         </View>
     )
 }

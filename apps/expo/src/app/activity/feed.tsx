20ced9668ee7c9cf96188b2556807d1ec04c3363
Thomas B <64014075+supakeshi@users.noreply.github.com>
2024-04-21 04:19:17 -0700

    44 1 frontend friends list screen structure (#215)
    * Initial commit of the friends list structure.
    
    Changes to be committed:
    	new file:   apps/expo/src/app/frlist/page.ts
    
    * renamed filetype to appropriate extension
    
    Changes to be committed:
    	renamed:    apps/expo/src/app/frlist/page.ts -> apps/expo/src/app/frlist/page.tsx
    
    * added route to friends list in nav
    
    Changes to be committed:
    	new file:   apps/expo/src/app/frlist/index.tsx
    	modified:   apps/expo/src/app/nav/index.tsx
    
    Changes not staged for commit:
    	deleted:    apps/expo/src/app/frlist/page.tsx
    	modified:   pnpm-lock.yaml
    
    * changed page.tsx to index.tsx
    
    * cleaned up frontend friends list screen structure
    TODO: Finish backend friend db and connect to frontend
    Changes to be committed:
    	modified:   apps/expo/src/app/frlist/index.tsx
    
    * created new api to get user friend list & changed to TRPc
    
    * added frlst to nav menu & def frontend methods
    
    * copied prisma schema from 178 branch
    
    * deleted loading screen
    
    * changed stylings and added expo image for avatars
    
    * comments redone
    
    * chaning the friend.ts for friendlist
    
    * api to get friends using logged in user id
    
    * Added Taylor's Search Bar Component
    
    * reimplemented Taylor's getFriend in friend.router.ts API
    
    * created a unit test file frlist_test.tsx
    
    * completed unit test for friend list
    
    * implemented the message router
    
    * got rid of spinning barbell in frlist load screen :-(
    
    * 219 frontendbackend refactor friends list page (#220)
    
    * 216 frontend modular nav bar (#217)
    
    * New nav bar + implementation into notif index.tsx
    
    * Additional usage comment
    
    * unit tests with 100% coverage
    
    * Fix component sizes
    
    * we got it cook?
    
    * computer kinda buggin rn i hope this doesnt crash
    
    * use upsert instead of create to make new chat if not found
    
    * delete old page perche ho dimenticato
    
    * deleted unused libs from ~/frlist/index.tsx
    
    * Finished unit tests for new frlist and components/Friend.tsx
    
    * prettier config'd test files
    
    * 🫠chorE: let me see
    
    * fixed activity feed to new frlist API
    
    * 🐧code: fix notif api and ABSOLUT path
    
    * 🐧code: searh bar test done
    
    * 🐧chore: fixed broken lock file
    
    * 👑code: letseee
    
    ---------
    
    Co-authored-by: Taylor Finelli <taylorfinelli25@gmail.com>
    Co-authored-by: Taylor Finelli <85365584+taylorfinelli@users.noreply.github.com>
    Co-authored-by: thienguen <nvktvan@gmail.com>

--- apps/expo/src/app/activity/feed.tsx
+++ apps/expo/src/app/activity/feed.tsx
@@ -5,22 +5,35 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { useGlobalContext } from '~/context/global-context'
 import { api } from '~/utils/trpc/api'
 import Activity from '.'
-import { ACTIVITY_FEED_ITEM_LIMIT } from '../../utils/constants'
+import { ACTIVITY_FEED_ITEM_LIMIT } from '~/utils/constants'
 
 const ActivityFeed = () => {
-  let activities: any[] = [];
-  const { userData } = useGlobalContext();
-  const { data: friends, isLoading: friendsIsLoading } = api.friend.getFriends.useQuery({ userId: userData?.id ?? 0 });
-  const { data: friendsWorkoutLogs, isLoading: friendsActivitiesLoading } = api.workout.getActivityFeedWorkouts
-          .useQuery(
-            { friendIds: friends?.map(friend => friend.friendId) ?? [], count: ACTIVITY_FEED_ITEM_LIMIT },
-            { enabled: !friendsIsLoading }
-          );
+  let activities: any[] = []
+  const { userData } = useGlobalContext()
+  const { data: friends, isLoading: friendsIsLoading } =
+    api.friend.getFriendsWithChatIdFromUserId.useQuery({
+      id: userData?.id ?? 0,
+    })
+  const { data: friendsWorkoutLogs, isLoading: friendsActivitiesLoading } =
+    api.workout.getActivityFeedWorkouts.useQuery(
+      {
+        friendIds: friends?.map((friend) => friend.id) ?? [],
+        count: ACTIVITY_FEED_ITEM_LIMIT,
+      },
+      { enabled: !friendsIsLoading },
+    )
 
   if (!friendsActivitiesLoading) {
-    activities = friendsWorkoutLogs?.map((workoutLog) => {
-      return <Activity user={workoutLog.user} workout={workoutLog.workout} workoutLog={workoutLog}></Activity>;
-    }) ?? [];
+    activities =
+      friendsWorkoutLogs?.map((workoutLog) => {
+        return (
+          <Activity
+            user={workoutLog.user}
+            workout={workoutLog.workout}
+            workoutLog={workoutLog}
+          ></Activity>
+        )
+      }) ?? []
   }
 
   return (

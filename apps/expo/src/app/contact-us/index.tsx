754292f1524482fb64b34c194088c4a2085565e6
Stefano Rubini <stefanorubini@Stefanos-MBP.lan>
2024-04-21 17:22:57 -0700

    Updated Contact Us page to include some of the suggestions from the comments that were possible.

--- apps/expo/src/app/contact-us/index.tsx
+++ apps/expo/src/app/contact-us/index.tsx
@@ -1,31 +1,37 @@
 import React from 'react'
-import { Linking, Text, View } from 'react-native'
+import { Linking, SafeAreaView, Text, View, ViewStyle } from 'react-native'
 
 import { Feather, Fontisto, SimpleLineIcons } from '@expo/vector-icons'
 
 import NavBar from '~/components/ui/nav-bar/NavBar'
 
+{
+  /* these variables are used to simplify common styling */
+}
+const viewStyles: ViewStyle = { flexDirection: 'row', alignItems: 'center', marginTop: 30 }
+const textStyles = { fontSize: 18, color: '#CACACA' }
+
 export default function ContactUsPage() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#1C1B1B' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#1C1B1B' }}>
       {/* Header */}
       <NavBar center='Contact Us' />
 
       <View style={{ flex: 1, paddingHorizontal: 20, paddingTop: 20 }}>
         {/* Email */}
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+        <View style={viewStyles}>
           <Fontisto name='email' size={24} color='#CACACA' style={{ marginRight: 10 }} />
-          <Text style={{ fontSize: 18, color: '#CACACA' }}>barbell_support@gmail.com</Text>
+          <Text style={textStyles}>barbell_support@gmail.com</Text>
         </View>
 
         {/* Phone */}
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
+        <View style={viewStyles}>
           <Feather name='phone' size={24} color='#CACACA' style={{ marginRight: 10 }} />
-          <Text style={{ fontSize: 18, color: '#CACACA' }}>+1 (702) 895-3011</Text>
+          <Text style={textStyles}>+1 (702) 895-3011</Text>
         </View>
 
         {/* Location */}
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 40 }}>
+        <View style={viewStyles}>
           <SimpleLineIcons
             name='location-pin'
             size={24}
@@ -33,17 +39,17 @@ export default function ContactUsPage() {
             style={{ marginRight: 10 }}
           />
           <View>
-            <Text style={{ fontSize: 18, color: '#CACACA' }}>University of Nevada, Las Vegas</Text>
-            <Text style={{ fontSize: 18, color: '#CACACA' }}>4505 South Maryland Parkway</Text>
-            <Text style={{ fontSize: 18, color: '#CACACA' }}>Las Vegas, NV 89154</Text>
+            <Text style={textStyles}>University of Nevada, Las Vegas</Text>
+            <Text style={textStyles}>4505 South Maryland Parkway</Text>
+            <Text style={textStyles}>Las Vegas, NV 89154</Text>
           </View>
         </View>
 
         {/* Response Time */}
-        <Text style={{ fontSize: 16, marginTop: 40, color: '#CACACA' }}>
-          Our goal is to respond to your inquiries within a 24-hour time frame on weekdays.
+        <Text style={{ ...textStyles, fontSize: 16, marginTop: 40, textAlign: 'center' }}>
+          Our goal is to respond to your inquiries within 24 business hours.
         </Text>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }

b446bf6cea627a3e0b9be9b0cca1bb41d9119946
Miguel <malvrez0525@gmail.com>
2024-04-24 04:47:34 -0700

    Finished prototype of graph. Ready to be further modified by rest of team to reach desired result.

--- apps/expo/src/components/workout/frequency_graph.tsx
+++ apps/expo/src/components/workout/frequency_graph.tsx
@@ -1,46 +1,34 @@
 import React from 'react'
 import {Text, View, Dimensions} from 'react-native'
 
-// Import bezier line graph from newly installed react-native-chart-kit library
-// Documentation: https://www.npmjs.com/package/react-native-chart-kit
-import { LineChart } from 'react-native-chart-kit'
-import { opacity } from 'react-native-reanimated/lib/typescript/reanimated2/Colors'
-import { Line } from 'react-native-svg'
+// Using the new chart library to see how it looks.
+// Properties: https://github.com/Abhinandan-Kushwaha/react-native-gifted-charts/blob/HEAD/docs/LineChart/LineChartProps.md
+import {LineChart} from 'react-native-gifted-charts'
 
 // To get the user's data, in particular the WorkoutLogs
 import { api } from '~/utils/trpc/api'
 import RotatingBarbellIcon from '../notif/RotatingBarbellIcon'
 
-enum MONTHS {
-    JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC
-}
-
 
 // Just use react-native-chart-kit to display the user's workout across each month for current year.
 export default function DisplayWorkoutFrequencyGraph({userID} : {userID: number}){
+    let values = [
+        {value: 0, label: "Jan"},
+        {value: 0, label: "Feb"},
+        {value: 0, label: "Mar"},
+        {value: 0, label: "Apr"},
+        {value: 5, label: "May"},
+        {value: 7, label: "Jun"},
+        {value: 4, label: "Jul"},
+        {value: 3, label: "Aug"},
+        {value: 2, label: "Sep"},
+        {value: 1, label: "Oct"},
+        {value: 2, label: "Nov"},
+        {value: 6, label: "Dec"},
+    ]
 
-    const chartConfig = {
-        backgroundGradientFrom: "#48476D",
-        backgroundGradientTo: "#48476D",
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-        propsForBackgroundLines: {
-    
-        }
-    };
-    let values = new Array<number>(12).fill(0);
-
-    const screenWidth = Dimensions.get("screen").width * 0.9
-    const screenHeight = Dimensions.get("screen").height * 0.3
-
-    // Psuedocode:
-    //  For object in returned data
-    //      Parse month and year
-    //      if year == currentYear:
-    //          switch(month) monthValue++;
-    // That populates the below values.
-
-    // Start by getting the current year. Shouldn't be too hard, just use date time
-    // Query the entirety of it and just store the value
+    const screenWidth = Dimensions.get("screen").width * 0.7
+    const screenHeight = Dimensions.get("screen").height * 0.2
 
     const {data: logData, isFetching} = api.user.getUserWorkoutHistory.useQuery({userId: userID});
 
@@ -51,38 +39,44 @@ export default function DisplayWorkoutFrequencyGraph({userID} : {userID: number}
             if(year===currYear){
                 // Valid log, check month and increment
                 let month:number = logData!.workoutHistory[i]!.finishedAt.getMonth() // Starts at 0 indexing for Jan
-                values[month]++
+                values[month]!.value++
             }
         }
     }
-    
-    const data = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov"," Dec"],
-        datasets: [
-            {
-                data: values,
-                color: (opacity = 1 ) => `rgba(255, 255, 255, ${opacity})`, // Optional
-                strokeWidth: 3
-            }
-        ],
-    };
+    // Find max value to make graph more dynamic
+    let max = 0
+    for(let i = 0; i < 12; i++){
+        if(max < values[i]!.value){
+            max = values[i]!.value
+        }
+    }
 
     return(
         <View>{isFetching ? <View><RotatingBarbellIcon /></View> : 
             <>
             <Text>Line Chart for User: {userID}</Text>
             <LineChart 
-                data = {data}
-                width = {screenWidth}
-                height = {screenHeight}
-                verticalLabelRotation={55}
-                chartConfig={chartConfig}
-                withShadow={false}
-                withInnerLines={false}
-                segments={5}
-                fromZero={true}
-                yLabelsOffset={15}
-                xLabelsOffset={-10}
+                rotateLabel={true}
+                curved
+                hideRules
+                curvature={0.2}
+                data={values}
+                height={screenHeight}
+                width={screenWidth}
+                spacing={25}
+                adjustToWidth={true}
+                startFillColor="#FFFFFF"
+                startOpacity={0.75}
+                endOpacity={0.1}
+                thickness={2.5}
+                color={"#FFFFFF"}
+                backgroundColor={"#48476D"}
+                dataPointsColor={"#FFFFFF"}
+                dataPointsRadius={4}
+                yAxisColor={"#48476D"}
+                xAxisColor={"#48476D"}
+                maxValue={max+5}
+                
             />
             </>
 }

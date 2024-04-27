107182cdcaa342cfb9723ec33b873f3950a5f326
mva919 <mva919@gmail.com>
2024-04-27 16:05:13 -0700

    Add hello message to dashboard

--- apps/expo/src/app/(dashboard)/index.tsx
+++ apps/expo/src/app/(dashboard)/index.tsx
@@ -3,23 +3,38 @@ import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { router } from 'expo-router'
 
+import RotatingBarbellIcon from '~/components/notif/RotatingBarbellIcon'
 import Button from '~/components/ui/button/button'
+import NavBar from '~/components/ui/nav-bar/NavBar'
+import { useGlobalContext } from '~/context/global-context'
 
-// FIXME: Dashboard start here
 const Dashboard = () => {
+  const { userData } = useGlobalContext()
+
   return (
     <SafeAreaView style={{ backgroundColor: '#1E1E1E', flex: 1 }}>
-      <View>
-        <Text className='text-center text-white'>Home screen</Text>
-      </View>
+      {userData ? (
+        <>
+          <NavBar center='Home' />
+          <View className='mt-4 px-4'>
+            <Text className='text-4xl font-semibold text-white'>
+              Hello {userData?.name.split(' ')[0]}!
+            </Text>
+          </View>
 
-      <Button
-        onPress={() => router.push('nav/')}
-        className='my-5 flex h-10 items-center rounded-md bg-blue-500'
-        aria-label='Go to nav'
-      >
-        <Text className='text-white'>Nav</Text>
-      </Button>
+          <Button
+            onPress={() => router.push('nav/')}
+            className='my-5 flex h-10 items-center rounded-md bg-blue-500'
+            aria-label='Go to nav'
+          >
+            <Text className='text-white'>Nav</Text>
+          </Button>
+        </>
+      ) : (
+        <View className='flex h-full items-center justify-center'>
+          <RotatingBarbellIcon />
+        </View>
+      )}
     </SafeAreaView>
   )
 }
